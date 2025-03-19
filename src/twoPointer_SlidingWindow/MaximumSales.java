11a698a50812eeba2d5db56b42eb367f4ae775e2
kodongwon <dw9706@gmail.com>
2025-03-19 15:55:43 +0900

    solve 최대 매출

---
+++ src/twoPointer_SlidingWindow/MaximumSales.java
@@ -0,0 +1,31 @@
+package twoPointer_SlidingWindow;
+// 3-3. 최대 매출
+
+import java.util.Scanner;
+public class MaximumSales {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] days = new int[n];
+        for (int i = 0; i < n; i++) {
+            days[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n, k, days));
+    }
+
+    public static int solution(int n, int k, int[] days) {
+        int maximum = 0;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += days[i];
+        }
+        maximum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum + days[i] - days[i - k];
+            maximum = Math.max(maximum, sum);
+        }
+        return maximum;
+    }
+}
