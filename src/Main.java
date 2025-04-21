b39fdcc30394008cee12151f75760d5b6efc9794
kodongwon <dw9706@gmail.com>
2025-04-22 08:40:34 +0900

    review 3-2. 공통원소 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,44 @@
-// 3-3. 최대 매출
+// 3-2. 공통원소 구하기
+
 
 import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] nums = new int[n];
-
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        Integer[] B = new Integer[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
-        int sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int Aptr = 0; int Bptr = 0;
+
+        while (Aptr < n && Bptr < m) {
+            if(A[Aptr].equals(B[Bptr])){
+                bw.write(A[Aptr]+" ");
+                Aptr++;
+                Bptr++;
+            }
+            else if(A[Aptr] < B[Bptr]) Aptr++;
+            else if(A[Aptr] > B[Bptr]) Bptr++;
         }
-        int answer = sum;
-        for (int i = k; i < n; i++) {
-            sum = sum + nums[i] - nums[i - k];
-            answer = Math.max(answer, sum);
-        }
-        System.out.println(answer);
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
 
