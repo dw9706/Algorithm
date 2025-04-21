0401380036cbc88829d7f167e9027afac287c99c
kodongwon <dw9706@gmail.com>
2025-04-21 18:33:41 +0900

    review 1-9. 숫자만 추출

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        int x = Integer.MIN_VALUE;
-        System.out.println(-x);
-        System.out.println(x);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if(Character.isDigit(c)) sb.append(c);
+        }
+        br.close();
+        System.out.println(Integer.parseInt(sb.toString()));
     }
 }
