094eda05774ba991afaf8208e70096b8fd5c3ed4
kodongwon <dw9706@gmail.com>
2025-04-21 20:01:35 +0900

    review 5-4. 후위식 연산

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
-// 1-2. 대소문자 변환
+//5-4. 후위식 연산
 
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] arr = sc.nextLine().toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-             if(Character.isUpperCase(arr[i])) arr[i] = Character.toLowerCase(arr[i]);
-             else arr[i] = Character.toUpperCase(arr[i]);
+        String postfix = sc.nextLine();
+        Stack<Integer> stack = new Stack<>();
+        for (char c : postfix.toCharArray()) {
+            if (Character.isDigit(c)) stack.push(Character.getNumericValue(c));
+            else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                int result = 0;
+                if (c == '+') {
+                    result = num1 + num2;
+                } else if (c == '-') {
+                    result = num1 - num2;
+                } else if (c == '/') {
+                    result = num1 / num2;
+                } else if (c == '*') {
+                    result = num1 * num2;
+                }
+                stack.push(result);
+            }
         }
-        System.out.println(String.valueOf(arr));
+        System.out.println(stack.pop());
     }
 }
+
