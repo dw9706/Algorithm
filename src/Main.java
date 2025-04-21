c1705491b708dd1b1cd952cbfc1c93ce74efe43a
kodongwon <dw9706@gmail.com>
2025-04-21 18:47:49 +0900

    review 1-4. 단어 뒤집기

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
-import java.util.*;
+// 1-4. 단어 뒤집기
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
-        for (char c : str.toCharArray()) {
-            if(Character.isDigit(c)) sb.append(c);
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            char[] arr = br.readLine().toCharArray();
+            int lt = 0;
+            int rt = arr.length - 1;
+            while (lt < rt) {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                lt++; rt--;
+            }
+            bw.write(String.valueOf(arr));
+            bw.write(System.lineSeparator());
         }
+
+        bw.flush();
         br.close();
-        System.out.println(Integer.parseInt(sb.toString()));
+        br.close();
     }
 }
