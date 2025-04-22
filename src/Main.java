25e7e1f3c83f6bccdee9d55daa5a9a69a95ae61c
kodongwon <dw9706@gmail.com>
2025-04-22 17:24:07 +0900

    review 1-5. 특정 문자 뒤집기

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
 // 1-3. 문장 속 단어
 
 
-import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.nextLine().toCharArray();
 
-        int max = 0;
-        String answer = "";
-
-        int len = st.countTokens();
-        for (int i = 0; i < len; i++) {
-            String tmp = st.nextToken();
-            if (max < tmp.length()) {
-                max = tmp.length();
-                answer = tmp;
-            }
+        int lt = 0;
+        int rt = arr.length - 1;
+        while (lt < rt) {
+            if (Character.isAlphabetic(arr[lt]) && Character.isAlphabetic(arr[rt])) {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                rt--; lt++;
+            } else if(!Character.isAlphabetic(arr[rt])) rt--;
+            else if(!Character.isAlphabetic(arr[lt])) lt++;
         }
-
-        System.out.println(answer);
-}
+        System.out.println(String.valueOf(arr));
+    }
 
 }
 
