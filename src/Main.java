3a577ff8b31fcf2c3b317773d64ef1308994b1f0
kodongwon <dw9706@gmail.com>
2025-04-22 17:49:34 +0900

    review 2-9. 격자판 최대합

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,41 @@
-// 1-6. 중복문자제거
+// 2-9. 격자판 최대합
 
 
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        HashSet<Character> set = new HashSet<>();
-        Scanner sc = new Scanner(System.in);
-        char[] arr = sc.nextLine().toCharArray();
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
 
-        for (char c : arr) {
-            if(set.contains(c)) continue;
-            set.add(c);
-            sb.append(c);
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        System.out.println(sb.toString());
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int sumRow = 0;
+            int sumCol = 0;
+            for (int j = 0; j < n; j++) {
+                sumRow += board[i][j];
+                sumCol += board[j][i];
+            }
+            answer = Math.max(answer, Math.max(sumRow, sumCol));
+        }
+
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += board[i][i];
+            sum2 += board[n - i - 1][i];
+        }
+
+        System.out.println(Math.max(answer, Math.max(sum1, sum2)));
+
 
     }
 
