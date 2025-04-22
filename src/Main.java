d3d3afbcbd67a3ca5600e54c4324c15f11de26a5
kodongwon <dw9706@gmail.com>
2025-04-22 17:10:41 +0900

    review 1-3. 문장 속 단어

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-// 10-4. 가장 높은 탑 쌓기
+// 1-3. 문장 속 단어
 
 
 import java.io.*;
@@ -6,47 +6,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Brick> bricks = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            bricks.add(new Brick(s, h, w));
-        }
-        Collections.sort(bricks);
-        int[] dy = new int[n];
-        dy[0] = bricks.get(0).h;
-        int answer = 0;
-        for (int i = 1; i < n; i++) {
-            int max = 0;
-            for (int j = i - 1; j >= 0; j--) {
-                if(bricks.get(i).w < bricks.get(j).w) max = Math.max(max, dy[j]);
-            }
-            dy[i] = max + bricks.get(i).h;
-            answer = Math.max(answer, dy[i]);
-        }
-        System.out.println(answer);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        br.close();
-    }
+        int max = 0;
+        String answer = "";
+
+        int len = st.countTokens();
+        for (int i = 0; i < len; i++) {
+            String tmp = st.nextToken();
+            if (max < tmp.length()) {
+                max = tmp.length();
+                answer = tmp;
+            }
+        }
+
+        System.out.println(answer);
 }
 
-class Brick implements Comparable<Brick> {
-    public int s;
-    public int h;
-    public int w;
-
-    public Brick(int s, int h, int w) {
-        this.s = s;
-        this.h = h;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(Brick o) {
-        return o.s - this.s;
-    }
 }
 
