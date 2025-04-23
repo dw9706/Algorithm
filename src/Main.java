95141f7f041c5db7b60d0bd32ce059c8389ac4f9
kodongwon <dw9706@gmail.com>
2025-04-23 11:04:34 +0900

    review 6-6. 장난꾸러기

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
-// 1-7. 회문 문자열
+//6-6. 장난 꾸러기
 
 
+import java.io.*;
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] arr = sc.nextLine().toUpperCase().toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String answer = "YES";
-        for (int i = 0; i < arr.length / 2; i++) {
-            if (arr[i] != arr[arr.length - i - 1]) {
-                answer = "NO";
-                break;
-            }
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(answer);
+        Integer[] sorted = arr.clone();
+        Arrays.sort(sorted);
 
+        for (int i = 0; i < n; i++) {
+            if(!arr[i].equals(sorted[i])) System.out.print((i + 1) + " ");
+        }
     }
-
 }
 
