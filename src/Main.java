80023249fc2caaae68d7077098a4691933440936
kodongwon <dw9706@gmail.com>
2025-04-23 10:33:25 +0900

    review 1-7. 회문 문자열

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//2-5. 소수
+// 1-7. 회문 문자열
 
 
 import java.util.*;
@@ -6,18 +6,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] check = new int[n + 1];
-        int answer = 0;
-        for (int i = 2; i <= n; i++) {
-            if (check[i] == 0) {
-                answer++;
-                for (int j = i; j <= n; j += i) {
-                    check[j] = 1;
-                }
+        char[] arr = sc.nextLine().toUpperCase().toCharArray();
+
+        String answer = "YES";
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - i - 1]) {
+                answer = "NO";
+                break;
             }
         }
+
         System.out.println(answer);
+
     }
 
 }
