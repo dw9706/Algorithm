9d7d2c530cff11bc368026045e1b5cb2b647af51
kodongwon <dw9706@gmail.com>
2025-04-25 18:19:40 +0900

    review 8-12. 토마토

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,63 @@
-//6-2. 버를 정렬
+//5-8. 응급실
 
 import java.util.*;
 import java.io.*;
+
 public class Main {
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] board = new int[m][n];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int tmp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = tmp;
+        Queue<Pos> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == 1) queue.offer(new Pos(i, j));
+            }
+        }
+        while (!queue.isEmpty()) {
+            Pos p = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if (0 <= nx && nx < m && 0 <= ny && ny < n && board[nx][ny] == 0) {
+                    queue.offer(new Pos(nx, ny));
+                    board[nx][ny] = board[p.x][p.y] + 1;
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.out.print(nums[i] + " ");
-        }
-
+        int answer = checkAnswer(n, m, board);
+        System.out.println(answer);
     }
+    public static int checkAnswer(int n, int m, int[][] board){
+        int answer = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(board[i][j] == 0) {
+                    return -1;
+                }
+                answer = Math.max(answer, board[i][j]);
+            }
+        }
+        return answer -1;
+    };
+
 }
 
+
+class Pos {
+    public int x;
+    public int y;
+
+    Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
