12c152d2840fc8747e0104a0964c375fb186a502
kodongwon <dw9706@gmail.com>
2025-04-25 18:42:54 +0900

    review 2-2. 보이는 학생

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//2-3. 가위 바위 보
+//2-2. 보이는 학생
 
 import java.util.*;
 import java.io.*;
@@ -7,21 +7,22 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        int[] b = new int[n];
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (max < nums[i]) {
+                answer++;
+                max = nums[i];
+            }
+        }
 
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st1.nextToken());
-            b[i] = Integer.parseInt(st2.nextToken());
-        }
-        for (int i = 0; i < n; i++) {
-            if(a[i] == b[i]) System.out.println("D");
-            else if (a[i] == 3 && b[i] == 1) System.out.println("B");
-            else if (b[i] == 3 && a[i] == 1) System.out.println("A");
-            else System.out.println(a[i] > b[i] ? "A" : "B");
-        }
+        System.out.println(answer);
+
     }
 }
 
