38fe310208ac7819a2f1de4cca1b923797419e00
kodongwon <dw9706@gmail.com>
2025-04-29 17:44:46 +0900

    review 2-10. 봉우리

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,44 @@
-//5-5. 쇠막대기
+// 2-10. 봉우리
 
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String sticks = sc.nextLine();
-        Stack<Character> stack = new Stack<>();
-        char pre = ' ';
-        int answer = 0;
-        for(char c : sticks.toCharArray()){
-            if(c == '(') stack.push(c);
-            else{
-                stack.pop();
-                if(pre == ')') answer++;
-                else{
-                    answer += stack.size();
-                }
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
-            pre = c;
+        }
+
+
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(isBonguri(board, i ,j)) answer++;
+            }
         }
         System.out.println(answer);
+    }
+
+    public static boolean isBonguri(int[][] board, int i, int j) {
+        int n = board.length;
+        for (int k = 0; k < 4; k++) {
+            int n_i = i + dx[k];
+            int n_j = j + dy[k];
+            if (0 <= n_i && n_i < n && 0 <= n_j && n_j < n) {
+                if (board[n_i][n_j] >= board[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
 
