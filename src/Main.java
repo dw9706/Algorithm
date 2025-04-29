04334589070f2694440f94b6b0eff601ed7cb116
kodongwon <dw9706@gmail.com>
2025-04-29 17:57:35 +0900

    review 2-8. 등수구하기

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,30 @@
-// 2-10. 봉우리
+// 2-8. 등수 구하기
 
 import java.util.*;
 import java.io.*;
 public class Main {
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] board = new int[n][n];
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-
-
-
-        int answer = 0;
+        int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
+            int cnt = 0;
             for (int j = 0; j < n; j++) {
-                if(isBonguri(board, i ,j)) answer++;
+                if(nums[i] < nums[j]) cnt++;
             }
+            answer[i] = cnt + 1;
         }
-        System.out.println(answer);
+        Arrays.stream(answer).forEach(i -> System.out.print(i+" "));
+
     }
 
-    public static boolean isBonguri(int[][] board, int i, int j) {
-        int n = board.length;
-        for (int k = 0; k < 4; k++) {
-            int n_i = i + dx[k];
-            int n_j = j + dy[k];
-            if (0 <= n_i && n_i < n && 0 <= n_j && n_j < n) {
-                if (board[n_i][n_j] >= board[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+
 }
 
 
